ea5b9da979a006aec583290fc62ff80150087604
hironobu0 <h.ohashi6491@gmail.com>
2020-09-08 00:16:19 +0900

    add graph and delete

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-let str = '';
-
+//JSONを作成
 let datalist = {
   key_num: '',
   name: '',
@@ -8,20 +7,26 @@ let datalist = {
   imp: '',
   taste: { hna: '', hjn: '', juk: '', oda: '', dry: '', kei: '' },
 };
-
+//セレクトボックスの作成
+let str = '';
 for (let i = 1; i <= 6; i++) {
   str += '<option>' + i + '</option>';
 }
-
 $('.taste_value').html(str);
 
+//Save, Clear後input項目を削除
 function emptyOut() {
   $('#name').val('');
   $('#meisan').val('');
   $('#shuzou').val('');
   $('#impression').val('');
+  location.reload();
+
+  myRadarChart.data.datasets[0].data = [0, 0, 0, 0, 0, 0];
+  myRadarChart.update();
 }
 
+//Saveをクリックしたらinput要素をJSON, localStorageに保存
 $('#save').on('click', function () {
   if ($('#name').val()) {
     datalist.key_num = localStorage.length;
@@ -37,12 +42,12 @@ $('#save').on('click', function () {
     datalist.taste.kei = $('#keikai').val();
 
     console.log(localStorage.length);
-
+    //JSONからキーを取得し、localStorageに保存
     localStorage.setItem(datalist.name, JSON.stringify(datalist));
     const getData = JSON.parse(localStorage.getItem(datalist.name));
     //一覧表示に追加
     const html =
-      '<tr><th>' +
+      '<tr class="add_list"><th>' +
       getData.name +
       '</th><td>' +
       getData.meisan +
@@ -53,7 +58,7 @@ $('#save').on('click', function () {
       '</td><td><p id= ' +
       getData.key_num +
       ' class = "graph">グラフ</p></td>' +
-      '</td><td><p class = "chg_record">編集</p></td></tr>';
+      '</td><td><p class = "del_record">削除</p></td></tr>';
 
     $('#list').append(html);
     emptyOut();
@@ -62,14 +67,15 @@ $('#save').on('click', function () {
   }
 });
 
+//一覧を削除
 $('#clear').on('click', function () {
   localStorage.clear();
-  $('#list').empty();
-  $('#myRaderChart').remove();
+  $('.add_list').empty();
 
   emptyOut();
 });
 
+//localStorageを読み込み一覧表示
 for (let i = 0; i < localStorage.length; i++) {
   const name = localStorage.key(i);
   const getData = JSON.parse(localStorage.getItem(name));
@@ -77,7 +83,9 @@ for (let i = 0; i < localStorage.length; i++) {
   const shuzou = getData.shuzou;
   const imp = getData.imp;
   const html =
-    '<tr><th>' +
+    '<tr id= ' +
+    i +
+    ' class="add_list"><th>' +
     name +
     '</th><td>' +
     meisan +
@@ -85,10 +93,8 @@ for (let i = 0; i < localStorage.length; i++) {
     shuzou +
     '</td><td>' +
     imp +
-    '</td><td><p id=' +
-    i +
-    ' class = "graph">グラフ</p></td>' +
-    '</td><td><p class = "chg_record">編集</p></td></tr>';
+    '</td><td><p class = "graph">グラフ</p></td>' +
+    '</td><td><p class = "del_record">削除</p></td></tr>';
   $('#list').append(html);
 }
 
@@ -99,55 +105,91 @@ for (let i = 0; i < localStorage.length; i++) {
 //   });
 // }
 
+//グラフ用のJSONを定義
+let getJsonData = {
+  key_num: '',
+  name: '',
+  meisan: '',
+  shuzou: '',
+  imp: '',
+  taste: { hna: '', hjn: '', juk: '', oda: '', dry: '', kei: '' },
+};
+
+//グラフを描写
+const ctx = document.getElementById('myRaderChart');
+const myRadarChart = new Chart(ctx, {
+  type: 'radar',
+  data: {
+    labels: ['華やか', '芳醇', '重厚', '穏やか', 'ドライ', '軽快'],
+    datasets: [
+      {
+        label: getJsonData.name,
+        data: [
+          getJsonData.taste.hna,
+          getJsonData.taste.hjn,
+          getJsonData.taste.juk,
+          getJsonData.taste.oda,
+          getJsonData.taste.dry,
+          getJsonData.taste.kei,
+        ],
+        backgroundColor: 'RGBA(225,95,150, 0.5)',
+        borderColor: 'RGBA(225,95,150, 1)',
+        borderWidth: 1,
+        pointBackgroundColor: 'RGB(46,106,177)',
+      },
+    ],
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'お酒の味わい',
+    },
+    scale: {
+      ticks: {
+        suggestedMin: 0,
+        suggestedMax: 6,
+        stepSize: 1,
+        callback: function (value, index, values) {
+          return value + '点';
+        },
+      },
+    },
+  },
+});
+
+//グラフボタンを押して味わいグラフを表示
+for (let i = 0; i < localStorage.length; i++) {
+  const tr_id = 'tr#' + i;
+
+  $(tr_id + ' .graph').on('click', function () {
+    const name = localStorage.key(i);
+    getJsonData = JSON.parse(localStorage.getItem(name));
+    myRadarChart.data.datasets[0].label = getJsonData.name;
+    myRadarChart.data.datasets[0].data = [
+      getJsonData.taste.hna,
+      getJsonData.taste.hjn,
+      getJsonData.taste.juk,
+      getJsonData.taste.oda,
+      getJsonData.taste.dry,
+      getJsonData.taste.kei,
+    ];
+    // console.log(myRadarChart.data.datasets[0].data);
+    myRadarChart.update();
+  });
+}
+//削除を押して行を削除
+for (let i = 0; i < localStorage.length; i++) {
+  const tr_id = 'tr#' + i;
+  $(tr_id + ' .del_record').on('click', function () {
+    console.log('クリック');
+    const name = localStorage.key(i);
+    localStorage.removeItem(name);
+    $('.del_record' + '#' + i).empty();
+    emptyOut();
+  });
+}
 //削除クリック→そのレコードのキーを取得→レコードを削除
 // $('#' + i).on('click', function () {
 //   let record = document.getElementById(i);
 //   localStorage.removeItem('urlBlackList');
 // });
-for (let i = 0; i < localStorage.length; i++) {
-  $('#' + i).on('click', function () {
-    const name = localStorage.key(i);
-    const getData = JSON.parse(localStorage.getItem(name));
-    const ctx = document.getElementById('myRaderChart');
-
-    const myRadarChart = new Chart(ctx, {
-      type: 'radar',
-      data: {
-        labels: ['華やか', '芳醇', '重厚', '穏やか', 'ドライ', '軽快'], //「華やか」「芳醇」「重厚」「穏やか」「ドライ」「軽快」
-        datasets: [
-          {
-            label: getData.name,
-            data: [
-              getData.taste.hna,
-              getData.taste.hjn,
-              getData.taste.juk,
-              getData.taste.oda,
-              getData.taste.dry,
-              getData.taste.kei,
-            ],
-            backgroundColor: 'RGBA(225,95,150, 0.5)',
-            borderColor: 'RGBA(225,95,150, 1)',
-            borderWidth: 1,
-            pointBackgroundColor: 'RGB(46,106,177)',
-          },
-        ],
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'お酒の味わい',
-        },
-        scale: {
-          ticks: {
-            suggestedMin: 0,
-            suggestedMax: 6,
-            stepSize: 1,
-            callback: function (value, index, values) {
-              return value + '点';
-            },
-          },
-        },
-      },
-    });
-  });
-}
